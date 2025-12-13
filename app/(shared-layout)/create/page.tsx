9e9274a7ab17772e8326ef5b0075301bb305eb2e
antonius-vanhaeren <antonius.vanhaeren.av@gmail.com>
2025-12-13 17:19:07 +0100

    feat: Implement blog post creation with new Convex schema and UI, replacing old task functionality.

---
+++ app/(shared-layout)/create/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import { postSchema } from "@/app/schemas/blog";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, Controller } from "react-hook-form";
+import z from "zod";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useTransition } from "react";
+import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+
+export default function CreatePage() {
+  const [isPending, startTransition] = useTransition()
+  const mutation = useMutation(api.posts.createPost)
+  const router = useRouter()
+
+  const form = useForm({
+    resolver: zodResolver(postSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+    }
+  })
+
+  function onSubmit(values: z.infer<typeof postSchema>) {
+    startTransition(() => {
+      mutation({
+        body: values.content,
+        title: values.title,
+      })
+
+      toast.success("Post created successfully")
+      router.push("/");
+    })
+  }
+
+  return (
+    <div className="py-12">
+      <div className="text-center mb-12">
+        <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">Create a new blog</h1>
+        <p className="text-xl text-muted-foreground pt-4">Share your thoughts with the world.</p>
+      </div>
+      <Card className="w-full max-w-xl mx-auto">
+        <CardHeader>
+          <CardTitle>Create Blog Article</CardTitle>
+          <CardDescription>Create a new blog article.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup className="gap-y-4">
+              <Controller name="title" control={form.control} render={({ field, fieldState }) =>
+                <Field>
+                  <FieldLabel>Title</FieldLabel>
+                  <Input aria-invalid={fieldState.invalid} placeholder="Title" {...field}></Input>
+                  {fieldState.invalid && (<FieldError errors={[fieldState.error]} />)}
+                </Field>
+              } />
+              <Controller name="content" control={form.control} render={({ field, fieldState }) =>
+                <Field>
+                  <FieldLabel>Content</FieldLabel>
+                  <Textarea aria-invalid={fieldState.invalid} placeholder="Content" {...field} />
+                  {fieldState.invalid && (<FieldError errors={[fieldState.error]} />)}
+                </Field>
+              } />
+              <Button type="submit" disabled={isPending}>{
+                isPending ? (
+                  <>
+                    <Loader2 className="size-4 animate-spin" />
+                    <span className="sr-only">Loading...  </span>
+                  </>
+
+                ) : <span>Create Post</span>
+
+              }</Button>
+
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
