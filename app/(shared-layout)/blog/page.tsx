d4512731aef7982410241092d695e97f93d00f7d
antonius-vanhaeren <antonius.vanhaeren.av@gmail.com>
2025-12-14 17:34:06 +0100

    feat: add skeleton loading UI and refactor blog page to use server-side data fetching with Suspense.

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,15 +1,13 @@
-"use client";
-
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
+import { fetchQuery } from "convex/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function BlogPage() {
-  const data = useQuery(api.posts.getPosts);
-  console.log(data);
   return (
     <div className="py-12">
       <div className="text-center pb-12">
@@ -18,28 +16,57 @@ export default function BlogPage() {
         </h1>
         <p className="pt-4 max-w-2xl mx-auto text-xl text-muted-foreground">Insights, thoughts, and ideas</p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data?.map((post) => (
-
-          <Card className="pt-0" key={post._id}>
-            <div className="relative h-48 w-full overflow-hidden rounded-t-lg ">
-              <Image src="https://images.unsplash.com/photo-1692087460128-da4bc008931c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="foto" fill className="object-cover" />
-            </div>
-            <CardContent>
-              <Link href={`/blog/${post._id}`}>
-                <h1 className="text-2xl font-semibold text-primary">{post.title}</h1>
-              </Link>
-              <p className="text-muted-foreground line-clamp-3">{post.body}</p>
-            </CardContent>
-            <CardFooter>
-              <Link className={buttonVariants({ className: "w-full", variant: "default" })} href={`/blog/${post._id}`}>
-                Read more
-              </Link>
-            </CardFooter>
-          </Card>
-
-        ))}
+      <div>
+        <Suspense fallback={<SkeletonLoadingUi />}>
+          <LoadBlogList />
+        </Suspense>
       </div>
     </div>
   );
+}
+
+async function LoadBlogList() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const data = await fetchQuery(api.posts.getPosts);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {data?.map((post) => (
+        <Card className="pt-0" key={post._id}>
+          <div className="relative h-48 w-full overflow-hidden rounded-t-lg ">
+            <Image src="https://images.unsplash.com/photo-1692087460128-da4bc008931c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="foto" fill className="object-cover" />
+          </div>
+          <CardContent>
+            <Link href={`/blog/${post._id}`}>
+              <h1 className="text-2xl font-semibold text-primary">{post.title}</h1>
+            </Link>
+            <p className="text-muted-foreground line-clamp-3">{post.body}</p>
+          </CardContent>
+          <CardFooter>
+            <Link className={buttonVariants({ className: "w-full", variant: "default" })} href={`/blog/${post._id}`}>
+              Read more
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+
+  )
+}
+
+function SkeletonLoadingUi() {
+  return (
+    <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-6">
+      {[...Array(3)].map((_, i) => (
+        <div key={i} className="flex flex-col space-y-3">
+          <Skeleton className="h-48 w-full rounded-xl" />
+          <div className="space-y-2 flex flex-col">
+            <Skeleton className="h-6 w-3/4 bg-primary" />
+            <Skeleton className="h-6 w-full bg-yellow-500" />
+            <Skeleton className="h-10 w-full bg-primary" />
+          </div>
+        </div>
+      ))}
+    </div >
+  )
 }
